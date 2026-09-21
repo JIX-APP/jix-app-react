3c560b6143ea72bbe0549e9c09da9066db53a126
JIX-APP <jilanisaddam3@gmail.com>
2026-09-22 01:27:05 +0300

    Update JixStreamStudio.tsx

--- JixStreamStudio.tsx
+++ JixStreamStudio.tsx
@@ -8,7 +8,6 @@ import { LevelBadge, useLevelXp } from './JixLevelSystem';
 import { JixLiveComments } from './JixLiveComments';
 import { JixModeratorManager } from './JixModeratorManager';
 import { JixCohostSlot } from './JixCohostSlot';
-import { JixLiveMvpBadge } from './JixMvpBadge';
 
 interface Viewer {
   id: string;
@@ -555,7 +554,6 @@ export const JixStreamStudio: React.FC<JixStreamStudioProps> = ({ isOpen, onClos
               </span>
             )}
             <LevelBadge xp={hostReceiverXp} kind="receiver" />
-            {liveId && <JixLiveMvpBadge liveId={liveId} size={32} />}
           </div>
 
           {isLive && (
