01eb484bfa755a3d6e244bcc83538b13510dd9bc
JIX-APP <jilanisaddam3@gmail.com>
2026-09-23 03:14:41 +0300

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { JixErrorBoundary } from './JixErrorBoundary.tsx';
+import { JixI18nProvider } from './JixLanguage.tsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <JixErrorBoundary>
-      <App />
+      {/* الترجمة تلف التطبيق كله عشان أي شاشة تقدر تستخدمها */}
+      <JixI18nProvider>
+        <App />
+      </JixI18nProvider>
     </JixErrorBoundary>
   </React.StrictMode>
 );
-
